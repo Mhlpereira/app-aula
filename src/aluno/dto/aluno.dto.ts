8776832439ff02d842d3d2a1721d3364975f5fe6
Mario Henrique <mariohenriquelp@gmail.com>
2024-08-30 11:36:05 -0300

    add validações para dto

--- src/aluno/dto/aluno.dto.ts
+++ src/aluno/dto/aluno.dto.ts
@@ -1,8 +1,18 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
 export class AlunoDTO{
+
     readonly id?: string;
+
+    @IsNotEmpty({ message: 'O campo vazio!'})
     readonly name: string;
+
+    @IsEmail(undefined, { message: "Email inválido!"}) //add validador de que é único tb
     readonly email: string;
+
+    @MinLength(6, {message:"Senha deve ter no mínimo 6 caracteres!"})
     readonly password: string;
-    readonly createAt: string;
-    readonly updateAt: string;
+    
+    readonly createAt?: string;
+    readonly updateAt?: string;
 }
